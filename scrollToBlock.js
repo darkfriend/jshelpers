1357337caf6e9aaab69dd83b6510a4f1f8258eb5
darkfriend <maslov.tlt@gmail.com>
2016-10-24 19:36:48 +0400

    add function scrollToBlock

---
+++ scrollToBlock.js
@@ -0,0 +1,16 @@
+/**
+ * @ru Скроллит по DOM-object
+ * @en It makes scroll to needed DOM-object
+ * @author darkfriend
+ * @param {string} selector
+ */
+function scrollToBlock(selector){
+    if($(selector).length){
+        var pos = $(selector).offset();
+        $('body').animate({scrollTop:pos.top}, 300);
+    } else {
+        if(typeof window.debug != 'undefined' && window.debug!==false){
+            console.log('Error! Selector: "'+selector+'" not found!');
+        }
+    }
+}
