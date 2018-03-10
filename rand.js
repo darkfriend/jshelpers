c0bf479ab87bab24f33bc27b06c6fc79c5da807c
darkfriend <maslov.tlt@gmail.com>
2018-03-10 21:44:16 +0400

    add jsdoc for rand()

--- rand.js
+++ rand.js
@@ -1,3 +1,9 @@
+/**
+ * Get random integer
+ * @param {int} min
+ * @param {int} max
+ * @returns {int}
+ */
 function rand(min=0,max=99999) {
     return Math.floor(Math.random() * Date.now() * (max - min + 1)) + min;
 }
