d47196ce3796ab733cfa57f7e60dae51cb34cefa
darkfriend <maslov.tlt@gmail.com>
2018-03-11 16:41:33 +0400

    добавил функцию для работы с числами

---
+++ number_forrmat.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * @param {number} number
+ * @param {int} decimals
+ * @param {string} dec_point
+ * @param {string} thousands_sep
+ * @returns {string}
+ */
+function number_format( number, decimals=2, dec_point=".", thousands_sep=" " ) {
+
+    let i, j, kw, kd, km;
+
+    // input sanitation & defaults
+    if( isNaN(decimals = Math.abs(decimals)) ){
+        decimals = 2;
+    }
+
+    i = parseInt(number = (+number || 0).toFixed(decimals)) + "";
+
+    if( (j = i.length) > 3 ){
+        j = j % 3;
+    } else{
+        j = 0;
+    }
+
+    km = (j ? i.substr(0, j) + thousands_sep : "");
+    kw = i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep);
+    kd = (decimals ? dec_point + Math.abs(number - i).toFixed(decimals).replace(/-/, 0).slice(2) : "");
+
+    return km + kw + kd;
+}
