be3b7c0548ae5cd103b33ee647e8749b2a719955
darkfriend <maslov.tlt@gmail.com>
2019-09-18 03:16:57 +0300

    set down error

--- empty.js
+++ empty.js
@@ -5,5 +5,9 @@
  * @return {boolean}
  */
 export function empty(variable) {
-	return ( variable === undefined || variable === "" || variable === 0   || variable === "0" || variable === null  || variable === false  || ( typeof variable === 'object' && Object.values(variable).length === 0) );
+	try {
+		return (variable === undefined || variable === "" || variable === 0 || variable === "0" || variable === null || variable === false || (typeof variable === 'object' && Object.values(variable).length === 0));
+	} catch (e) {
+		return true;
+	}
 }
